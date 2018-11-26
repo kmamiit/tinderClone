55a77df94aed2640f41d35dd2402ca24e12f2187
kmamiit <kmamiit123@gmail.com>
2018-11-25 20:58:43 -0800

    Initial Commit

---
+++ tinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tinderClone
+//
+//  Created by Kyle Mamiit (New) on 11/25/18.
+//  Copyright © 2018 Kyle Mamiit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
